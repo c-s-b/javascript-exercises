a39789d791de0ea03e4bba602f36162b2a5d0350
Casey Brock <casey.brock613@gmail.com>
2022-07-16 18:00:35 -0400

    passed all 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    reversedArray = [];
+    let newString = "";
 
+    for ( let i = (stringArray.length - 1 ); i >= 0; i-- ) {
+        reversedArray.push(stringArray[i]);
+        
+    }
+    newString = reversedArray.join("");
+    return newString;
+    
 };
 
 // Do not edit below this line
