925ef25a00a71d045e3cbb7bf7e78ef77ab400eb
Casey Brock <casey.brock613@gmail.com>
2022-07-16 23:12:59 -0400

    Pass Tests - Notice toFixed was faster. machine factor?

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+toCelsius = Math.round( (temp - 32) * (5/9) * 10) /10;
 
+return toCelsius;
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+toFahrenheit = Math.round( (temp * (9/5) + 32) * 10) /10;
 
+return toFahrenheit;
 };
 
 // Do not edit below this line
