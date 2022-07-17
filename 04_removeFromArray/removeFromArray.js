6e6571b2219b536d54aff6791498ff5647d3d76d
Casey Brock <casey.brock613@gmail.com>
2022-07-16 20:14:09 -0400

    Pass All Tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    const newArray = [];
+    outer:
+    for(i = 0; i < array.length; i++) {
 
+        for(j = 0; j < values.length; j++){
+            if(array[i] === values[j]) {
+                continue outer;
+            }
+        }
+
+        newArray.push(array[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
