0f6ce0984f95c01e8e48ee1c8eab28b0c3404c5f
Casey Brock <casey.brock613@gmail.com>
2022-07-16 22:16:08 -0400

    Pass All Tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(first , second) {
+let sum = 0;
+
+let checkNumbers = () => {
+    if (typeof first === 'number' && typeof second === 'number' && first > 0 && second > 0){
+    return true;
+    } else {
+        return false;
+    }  
+}
+
+ if(checkNumbers()) {
+    let start = Math.min( first , second );
+    let end = Math.max (first , second);
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
+} else {
+    return "ERROR"
+}
+
 
 };
+console.log(sumAll(10,4))
 
 // Do not edit below this line
 module.exports = sumAll;
